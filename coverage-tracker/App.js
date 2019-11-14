292415f8b00481643300bf7b901fc43df09be8dd
Jackson Kennedy <jtk5aw@virginia.edu>
2019-11-14 18:18:41 -0500

    Fetching all Data

--- coverage-tracker/App.js
+++ coverage-tracker/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  // Setting up locations
+  const [locations, setLocations] = useState([]); 
+
+  //Hook The fires on onmount and gets data 
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch(
+        "https://api.devhub.virginia.edu/v1/facilities/categories/housing"
+      );
+      let parseObject = await response.json();
+      setLocations(assignIDs(parseObject))
+      console.log(locations[0])
+    }
+    fetchData()
+  }, []);
+
+  function assignIDs(locations){
+    // ONly keeps locationsn with valid locations
+    return locations.map((location, index)=>{
+      location.id = index
+      return location
+    }).filter(location => location.Latitude && location .Longitude)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        {
+          locations.map((location) => {
+            return <Text key={location.id}>Name: {location.Name} Lat: {location.Latitude} Long: {location.Longitude}</Text>
+          })
+        }
+      </ScrollView>
     </View>
   );
 }
