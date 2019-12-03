8f9fbc3259ff33e5810a5875ad05456a24fc0ceb
Jackson Kennedy <jtk5aw@virginia.edu>
2019-12-03 02:07:47 -0500

    Fix indentation

--- coverage-tracker/components/HomeScreen.js
+++ coverage-tracker/components/HomeScreen.js
@@ -55,35 +55,35 @@ export default function HomeScreen(props) {
     getStartLoc()
   }, [])
 
-    useEffect(() => {
-      dormLocationsRef.get()
-      .then((querySnapshot) => {
-        parseObject = querySnapshot.docs.map((doc, index) => {
-          data = doc.data()
-          return {
-            'id': index,
-            'Name': doc.id,
-            'Latitude': data.Latitude,
-            'Longitude': data.Longitude
-          }
-        })
-        setLocations(parseObject)
+  useEffect(() => {
+    dormLocationsRef.get()
+    .then((querySnapshot) => {
+      parseObject = querySnapshot.docs.map((doc, index) => {
+        data = doc.data()
+        return {
+          'id': index,
+          'Name': doc.id,
+          'Latitude': data.Latitude,
+          'Longitude': data.Longitude
+        }
       })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-      });
+      setLocations(parseObject)
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
     
-    }, [])
+  }, [])
 
-    const signout = () => {
-        firebase.auth().signOut().then(() => {
-            props.navigation.navigate('Login', {
-                navigation: props.navigation,
-            })
-        }).catch((error) =>  {
-            alert('An error occurred that prevented Sign Out from occuring')
+  const signout = () => {
+    firebase.auth().signOut().then(() => {
+        props.navigation.navigate('Login', {
+            navigation: props.navigation,
         })
-    }
+    }).catch((error) =>  {
+        alert('An error occurred that prevented Sign Out from occuring')
+    })
+  }
 
   return (
     <View style={styles.absoluteFill}>
